1bf823beb8aad40a1bc512f7830716fdfc66f54b
Steven Ciasullo <sjciasullo@gmail.com>
2017-10-11 16:10:10 -0400

    add hover styles for menu and add level buttons

--- js/main.js
+++ js/main.js
@@ -1,1 +1,20 @@
- console.log('main.js is running');
+console.log('main.js is running');
+
+
+
+let buttons = document.querySelectorAll('button');
+buttons[0].addEventListener('mouseover', () => {
+  document.getElementById('level').style.color = 'red';
+ });
+buttons[0].addEventListener('mouseout', () => {
+  document.getElementById('level').style.color = 'white';
+ });
+
+
+
+buttons[1].addEventListener('mouseover', () => {
+  document.getElementById('level').style.color = 'red';
+ });
+buttons[1].addEventListener('mouseout', () => {
+  document.getElementById('level').style.color = 'white';
+ });
